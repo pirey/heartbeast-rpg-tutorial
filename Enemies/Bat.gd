a4ad2cb3a07567f3266c1b12ef5e4059cb0d667a
Yeri <arifyeripratama@gmail.com>
2020-11-18 14:05:29 +0700

    P14 | Enemy Stats + Export Variables + Setget

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+onready var stats = $Stats
+
 var knockback = Vector2.ZERO
 
 func _physics_process(delta):
@@ -7,4 +9,8 @@ func _physics_process(delta):
 	knockback = move_and_slide(knockback)
 
 func _on_Hurtbox_area_entered(area):
+	stats.health -= area.damage
 	knockback = area.knockback_vector * 120
+
+func _on_Stats_no_health():
+	queue_free()
